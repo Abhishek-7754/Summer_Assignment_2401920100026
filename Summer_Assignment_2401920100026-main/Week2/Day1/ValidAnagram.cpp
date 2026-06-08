4c6b0c0eb3d79852f200dd4a0ea3ef4db7ddfb20
Abhishek-7754 <abhipaswan7754@gmail.com>
2026-06-08 17:44:04 +0530

    Added Week2 Day1 Valid Anagram solution

---
+++ Summer_Assignment_2401920100026-main/Week2/Day1/ValidAnagram.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool isAnagram(string s, string t) {
+      if(s.size()!= t.size()) return false;
+      int arr[26] = {0};
+
+      for(int i=0; i<s.size();i++){
+        arr[s[i]-'a']++;
+        arr[t[i]-'a']--;
+      }
+      for(int check: arr){
+        if(check!=0) return false;
+
+      }
+       return true; 
+    }
+
+int main() {
+    string s, t;
+    cout << "Enter the first string: ";
+    cin >> s;
+    cout << "Enter the second string: ";
+    cin >> t;
+
+    if (isAnagram(s, t)) {
+        cout << "anagrams" << endl;
+    } else {
+        cout << "not anagrams" << endl;
+    }
+
+    return 0;
+}
+   
