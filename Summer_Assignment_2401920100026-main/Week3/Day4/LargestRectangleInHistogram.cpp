9b471b44eebc7bb7e3a81a42445189d5dc14bc83
Abhishek-7754 <abhipaswan7754@gmail.com>
2026-06-18 15:29:49 +0530

    Added Week3 day4 solution

---
+++ Summer_Assignment_2401920100026-main/Week3/Day4/LargestRectangleInHistogram.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int largestRectangleArea(vector<int>& heights) {
+          int n = heights.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            left[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+    while (!st.empty()) st.pop();
+for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            right[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            maxArea = max(maxArea, heights[i] * width);
+        }
+        return maxArea;
+    }
+    int main() {
+    vector<int> heights = {2, 1, 5, 6, 2, 3};
+
+    cout << largestRectangleArea(heights) << endl;
+
+    return 0;
+}
